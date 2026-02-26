41e57998e8733624162626ab3b45972d0b756806
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-02-26 16:03:38 -0500

    lua stuff ig

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,27 @@
 #include "raylib.h"
 
 #include "funkin/Game.hpp"
+#include "luaaa.hpp"
+#include <iostream>
 
-int main() {
+int main()
+{
+
+    lua_State *L = luaL_newstate();  // Create a new Lua state
+    luaL_openlibs(L);                 // Load Lua libraries
+    luaL_dostring(L, "print('Hi from lua!')"); // Execute Lua script
+    lua_close(L);                     // Close the Lua state
+	
 	InitWindow(1280, 720, "Friday Night Funkin'");
 	InitAudioDevice();
 
 	SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()) * 2);
 
+
 	funkin::Game::start(std::make_unique<funkin::scenes::PlayScene>());
 
-	while (!WindowShouldClose()) {
+	while (!WindowShouldClose())
+	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		funkin::Game::update(GetFrameTime());
