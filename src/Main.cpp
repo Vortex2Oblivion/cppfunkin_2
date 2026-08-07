22b0e89f4d4f7185359f475de1dfce8dd778b08f
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-08-07 19:37:24 -0400

    dark mode header on windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,11 @@
 #include "funkin/scenes/TitleScene.hpp"
 #include "raylib.h"
 
+#ifdef _WIN32
+#include "external/fix_win32_compatibility.h"
+#include "dwmapi.h"
+#endif
+
 int main() {
 
 	constexpr int windowWidth = 1280;
@@ -10,6 +15,16 @@ int main() {
 	constexpr auto windowTitle = "Friday Night Funkin'";
 
 	InitWindow(windowWidth, windowHeight, windowTitle);
+
+#ifdef _WIN32
+	constexpr int darkMode = 1;
+
+	const auto window = static_cast<HWND>(GetWindowHandle());
+	// ReSharper disable once CppFunctionResultShouldBeUsed
+	DwmSetWindowAttribute(window, 20, &darkMode, sizeof(darkMode));
+	UpdateWindow(window);
+#endif
+
 	InitAudioDevice();
 
 	SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()) * 2);
