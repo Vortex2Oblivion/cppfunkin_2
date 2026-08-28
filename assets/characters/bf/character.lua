b7b377dd12c66032ac83365028d2f5ae7735522d
Steph <38338700+superpowers04@users.noreply.github.com>
2026-08-28 10:28:17 -0400

    bf flip

--- assets/characters/bf/character.lua
+++ assets/characters/bf/character.lua
@@ -1,4 +1,8 @@
 function onCreateCharacter(character)
+
+    character.flipX = (character.type==1)
+
+
     character.animation:addByPrefix("idle" ,"BF idle dance", 24, false, {})
     character.animation:addOffset("idle", -5, 0)
 
@@ -8,15 +12,16 @@ function onCreateCharacter(character)
     character.animation:addByPrefix("singUP" ,"BF NOTE UP0", 24, false, {})
     character.animation:addOffset("singUP", -46, 27)
 
-    character.animation:addByPrefix("singRIGHT" ,"BF NOTE RIGHT0", 24, false, {})
-    character.animation:addOffset("singRIGHT", -48, -7)
+    local right = flipX and "singLEFT" or "singRIGHT"
+    character.animation:addByPrefix(right ,"BF NOTE RIGHT0", 24, false, {})
+    character.animation:addOffset(right, -48, -7)
 
-    character.animation:addByPrefix("singLEFT" ,"BF NOTE LEFT0", 24, false, {})
-    character.animation:addOffset("singLEFT", 5, -6)
+    local left = flipX and "singRIGHT" or "singLEFT"
+    character.animation:addByPrefix(left ,"BF NOTE LEFT0", 24, false, {})
+    character.animation:addOffset(left, 5, -6)
 
     character.animation:play("idle", true)
 
-    character.flipX = (character.type==1)
 
     character.barColor = Color.new(49, 176, 209, 255)
 end
